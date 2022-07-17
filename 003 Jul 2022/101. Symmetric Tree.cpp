41c8c0570f99078ae0cb0e694062c3ecb895fd36
Amit Maity <maityamit308@gmail.com>
2022-07-18 01:52:44 +0530

    Create 101. Symmetric Tree.cpp

---
+++ 003 Jul 2022/101. Symmetric Tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+        return helper(root->left,root->right);
+    }
+    bool helper(TreeNode* left,TreeNode* right){
+        if(left==NULL || right==NULL){
+            return left==right;
+        }
+        if(left->val!=right->val){
+            return false;
+        }
+        return helper(left->left,right->right)&&helper(left->right,right->left);
+    }
+};
