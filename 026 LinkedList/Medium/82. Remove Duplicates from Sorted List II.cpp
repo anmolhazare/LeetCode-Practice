f2cb1555d31679c4709d8c7628915424b5a1d326
Amit Maity <maityamit308@gmail.com>
2022-09-20 11:05:22 +0530

    Update 82. Remove Duplicates from Sorted List II.cpp

--- 026 LinkedList/Medium/82. Remove Duplicates from Sorted List II.cpp
+++ 026 LinkedList/Medium/82. Remove Duplicates from Sorted List II.cpp
@@ -10,53 +10,32 @@
  */
 class Solution {
 public:
-    pair<ListNode* ,ListNode* > reverse(ListNode* head){
-        if(head==NULL || head->next==NULL){
-            pair<ListNode* , ListNode*> p;
-            p.first=head;
-            p.second=head;
-            return p;
-        }
-        pair<ListNode* , ListNode* > temp = reverse(head->next);
-        temp.second->next=head;
-        head->next=NULL;
-        temp.second=head;
-        return temp;
-    }
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(head==NULL || head->next==NULL){
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL){
             return head;
         }
+        ListNode* temp = head;
         ListNode* start = NULL;
         ListNode* end = NULL;
-        ListNode* prev = NULL;
-        ListNode* temp = head;
-        int count=1;
-        if(left==1){
-            start=head;
+        unordered_map<int,int> seen;
+        while(temp!=NULL){
+            seen[temp->val]++;
+            temp=temp->next;
         }
-        while(temp->next!=NULL){
-            if((count+1)==left){
-                prev=temp;
-                start=temp->next;
-            }
-            if(count==right){
-                end=temp->next;
-                temp->next=NULL;
-                break;
+        temp=head;
+        while(temp!=NULL){
+            if(seen[temp->val]==1){
+                ListNode* amit = new ListNode(temp->val);
+                if(start==NULL){
+                    start=amit;
+                    end=amit;
+                }else{
+                    end->next=amit;
+                    end=end->next;
+                }
             }
             temp=temp->next;
-            count++;
         }
-        pair<ListNode* , ListNode* > tempu = reverse(start);
-        if(tempu.first!=NULL){
-            if(prev==NULL){
-                 tempu.second->next=end; 
-                 return tempu.first;
-            }
-         prev->next=tempu.first;
-         tempu.second->next=end;   
-        }
-        return head;
+        return start;
     }
 };
