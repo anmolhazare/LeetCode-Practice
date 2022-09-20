9222d02ac31c9aed58b9081ccd6a81db06aae2e7
Amit Maity <maityamit308@gmail.com>
2022-09-20 11:20:10 +0530

    Create 92. Reverse Linked List II.cpp

---
+++ 026 LinkedList/Medium/92. Reverse Linked List II.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    pair<ListNode* ,ListNode* > reverse(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            pair<ListNode* , ListNode*> p;
+            p.first=head;
+            p.second=head;
+            return p;
+        }
+        pair<ListNode* , ListNode* > temp = reverse(head->next);
+        temp.second->next=head;
+        head->next=NULL;
+        temp.second=head;
+        return temp;
+    }
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* start = NULL;
+        ListNode* end = NULL;
+        ListNode* prev = NULL;
+        ListNode* temp = head;
+        int count=1;
+        if(left==1){
+            start=head;
+        }
+        while(temp->next!=NULL){
+            if((count+1)==left){
+                prev=temp;
+                start=temp->next;
+            }
+            if(count==right){
+                end=temp->next;
+                temp->next=NULL;
+                break;
+            }
+            temp=temp->next;
+            count++;
+        }
+        pair<ListNode* , ListNode* > tempu = reverse(start);
+        if(tempu.first!=NULL){
+            if(prev==NULL){
+                 tempu.second->next=end; 
+                 return tempu.first;
+            }
+         prev->next=tempu.first;
+         tempu.second->next=end;   
+        }
+        return head;
+    }
+};
