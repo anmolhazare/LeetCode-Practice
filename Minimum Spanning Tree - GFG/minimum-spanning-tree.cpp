c8d75a178e61aa2d78646f4fb247822f5750fd28
Amit Maity <maityamit308@gmail.com>
2022-11-28 15:37:16 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,6 +3,59 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet{
+	vector<int> rank,parent,size;
+	public:
+		DisjointSet(int n){
+			rank.resize(n+1,0);
+			size.resize(n+1,1);
+			parent.resize(n+1);
+			for(int i=0;i<=n;i++) parent[i]=i;
+		}
+		
+		int findUltimateParent(int node){
+			if(node==parent[node]){
+				return node;
+			}
+			return parent[node] = findUltimateParent(parent[node]);
+		}
+		
+		void unionByRank(int u,int v){
+			
+			int ulp_u = findUltimateParent(u);
+			int ulp_v = findUltimateParent(v);
+			
+			if(ulp_u==ulp_v) return;
+			
+			if(rank[ulp_u] < rank[ulp_v]){
+				parent[ulp_u] = ulp_v;
+			}else if(rank[ulp_v] < rank[ulp_u]){
+				parent[ulp_v] = ulp_u;
+			}else{
+				parent[ulp_v]=ulp_u;
+				rank[ulp_u]++;
+			}
+		}
+		
+		void unionBySize(int u,int v){
+			
+			int ulp_u = findUltimateParent(u);
+			int ulp_v = findUltimateParent(v);
+			
+			if(ulp_u==ulp_v) return;
+			
+			if(size[ulp_u] < size[ulp_v]){
+				parent[ulp_u] = ulp_v;
+				size[ulp_v] += size[ulp_u];
+			}else{
+				parent[ulp_v] = ulp_u;
+				size[ulp_u] += size[ulp_v];
+			}
+			
+		}
+		
+};
 class Solution
 {
 	public:
@@ -10,38 +63,31 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int adjNode = it[0];
+                int wt = it[1];
+                edges.push_back({wt,{i,adjNode}});
+            }
+        }
+        sort(edges.begin(),edges.end());
         
-        
-        // code here
         int sum = 0;
-        vector<bool> vis(V,false);
-        priority_queue<pair<int,pair<int,int>>, 
-        vector<pair<int,pair<int,int>>>, 
-        greater<pair<int,pair<int,int>>>> pq;
-        
-        pq.push({0,{0,-1}});
-        while(pq.size()!=0){
-            int wt = pq.top().first;
-            int node = pq.top().second.first;
-            int parent = pq.top().second.second;
-            pq.pop();
+        DisjointSet ds(V);
+        for(auto it:edges){
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
             
-            if(vis[node]==true) continue;
-            
-            vis[node]=true;
-            sum+=wt;
-            
-            for(auto it:adj[node]){
-                if(!vis[it[0]]){
-                    pq.push({it[1],{it[0],node}});
-                }
+            if(ds.findUltimateParent(u)!=ds.findUltimateParent(v)){
+                sum+=wt;
+                ds.unionBySize(u,v);
             }
             
         }
         
         return sum;
-        
-        
     }
 };
 
